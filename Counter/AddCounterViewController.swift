51e9458e559737ba331c38dda59206be9b99c06a
Kassie <ksiuwng@gmail.com>
2018-05-16 16:14:48 -0700

    added delete function added enable/disable save features

--- Counter/AddCounterViewController.swift
+++ Counter/AddCounterViewController.swift
@@ -16,12 +16,33 @@ class AddCounterViewController: UIViewController {
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var descText: UITextField!
     @IBOutlet weak var countText: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // MARK: How to enable/disable button
+        // 1. Set button to isEnabled false
+        // 2. addTargets
+        // 3. make textFieldDidChange func
+        saveButton.isEnabled = false // disable save button until fields have something in it
+        nameText.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        countText.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        
+    }
+    
+    @objc func textFieldDidChange (_ textField:UITextField) {
+        if let name = nameText.text, let count = countText.text {
+            if !name.isEmpty, !count.isEmpty {
+                saveButton.isEnabled = true
+            } else {
+                saveButton.isEnabled = false
+            }
+
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,21 +66,18 @@ class AddCounterViewController: UIViewController {
     }
     
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
-//        let appDelegate = (UIApplication.shared.delegate as! AppDelegate).persist
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-//        let counterItem = Counters(context: context)
         let counterItem = Counters(context: context)
-//
-        counterItem.name = nameText.text!
-////        counterItem.desc = descText.text?
-////        counterItem.counter = countText.text!
-//
-        print("Pressed")
+
+        
+        counterItem.name = nameText.text
+        counterItem.count = Int64(countText.text!)!
+        
         appDelegate.saveContext()
         self.dismiss(animated: true, completion: nil)
     }
-    
+
 
 }
--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -36,21 +36,12 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    // MARK: SET UP CELLS
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return counters.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "counterCell", for: indexPath) as? CounterTableViewCell else {
-//            fatalError("No.")
-//        }
-//
-//        cell.countLabel.text = "3"
-//        cell.nameLabel.text = "School end"
-//        cell.descLabel.text = "School is ending!!!"
-//
-//        return cell
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "counterCell", for: indexPath) as? CounterTableViewCell else {
             fatalError("No.")
@@ -58,6 +49,7 @@ class ViewController: UITableViewController {
         
         let counter = counters[indexPath.row]
         cell.nameLabel.text = counter.name!
+        cell.countLabel.text = String(counter.count)
 
         return cell
         
@@ -72,6 +64,30 @@ class ViewController: UITableViewController {
             print("Fetching failed")
         }
     }
+    
+    // MARK: EDIT CELLS
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCellEditingStyle.delete) {
+            // delete
+            print("Delete")
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            
+            let commit = counters[indexPath.row]
+            
+            appDelegate.persistentContainer.viewContext.delete(commit)
+
+            counters.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            appDelegate.saveContext()
+            
+        }
+    }
+    
 
 
 }
